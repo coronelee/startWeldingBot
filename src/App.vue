922ff9736e82e68ad0706c0c9f059f28fbacd9b1
coronelee <a.tirki90@gmail.com>
2024-06-13 08:25:00 +0300

    add

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <div id="app"></div>
+</template>
+
+<script setup>
+import { TelegramWebAppContainer } from '@telegram-web-app/core';
+
+const telegram = new TelegramWebAppContainer();
+
+telegram.WebApp.ready();
+telegram.WebApp.expand();
+telegram.WebApp.openTelegramLink('https://t.me/welding_seams_bot/WeldingSeamsApp');
+
+</script>
